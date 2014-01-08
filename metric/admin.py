5c1a662a8b23c4604a93ccdf5118c384d4b25bb6
Golden Murray <gmurray@adaptivecomputing.com>
2014-01-08 10:20:06 -0700

    Changed procedure table to use_case, changed samples date field to datetime

--- metric/admin.py
+++ metric/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-#from metric.models import Environment, Metric, Procedure, Sample
+#from metric.models import Environment, Metric, UseCase, Sample
 from metric.models import Metric, Sample
 
 #admin.site.register(Environment)
 admin.site.register(Metric)
-#admin.site.register(Procedure)
+#admin.site.register(UseCase)
 admin.site.register(Sample)
 
 
