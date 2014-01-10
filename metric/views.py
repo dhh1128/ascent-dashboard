5628280bc77aa680265a509313fc320198da9312
Golden Murray <gmurray@adaptivecomputing.com>
2014-01-10 15:00:10 -0700

    Changed display to handle new data model with use cases.  Now generates multiple graphs, one for each use case

--- metric/views.py
+++ metric/views.py
@@ -5,34 +5,43 @@ import json
 #import logging
 
 from models import Sample
+from models import UseCase
 
 #log = logging.getLogger('django.db.backends')
 #log.setLevel(logging.DEBUG)
 #log.addHandler(logging.StreamHandler())
 
-
 def index(request):
     template = loader.get_template('metric/index.html')
 
-    metrics = []
+    usecases = []
     now = datetime.now()
-    for sample in Sample.objects.filter(sample_date__range=(now -
-                                        timedelta(days=90),
-                                        now)).select_related():
-        name = sample.metric.name
-        metric = filter(lambda m: m['name'] == name, metrics)
-        if len(metric):
-            metric = metric[0]
-        else:
-            metric = {'name': name, 'samples': [],
-                      'units': sample.metric.units, 'url': sample.metric.explanation_url}
-            metrics.append(metric)
+    #for sample in Sample.objects.filter(sample_date__range=(now -
+    #                                    timedelta(days=90),
+    #                                    now)).filter(use_case_id__exact=1).select_related():
+    for usecase in UseCase.objects.all():
+        metrics = []
+        metricName = ""
+        for sample in Sample.objects.filter(sample_date__range=(now -
+                                            timedelta(days=90),
+                                            now)).filter(use_case_id__exact=usecase.id).select_related():
+            metricName = sample.metric.name
+            metric = filter(lambda m: m['name'] == metricName, metrics)
+            if len(metric):
+                metric = metric[0]
+            else:
+                metric = {'name': metricName,
+                          'samples': [],
+                          'units': sample.metric.units, 'url': sample.metric.explanation_url}
+                metrics.append(metric)
 
-        metric['samples'].append({'date': sample.sample_date.strftime('%Y-%m-%dT%H:%M:%S'),
-                                  'value': sample.value})
+            metric['samples'].append({'date': sample.sample_date.strftime('%Y-%m-%dT%H:%M:%S'),
+                                      'value': sample.value})
+
+        usecases.append({ 'id': usecase.id, 'name' :usecase.name, 'metrics': metrics })
 
     context = RequestContext(request, {
-        'metrics': json.dumps(metrics)
+        'usecases': json.dumps(usecases)
     })
 
     return HttpResponse(template.render(context))
